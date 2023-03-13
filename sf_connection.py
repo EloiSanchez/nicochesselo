7e69b5868b7e15f16701f8ff05f8640288ebf97a
Niccolo Racioppi <nicsputnik@gmail.com>
2023-03-13 11:45:05 +0100

    Create module sf_connection

---
+++ sf_connection.py
@@ -0,0 +1,12 @@
+import snowflake.connector
+import os
+
+os.environ["SNOWSQL_USR"] = ""
+os.environ["SNOWSQL_PWD"] = ""
+os.environ["SNOWSQL_ACC"] = ""
+os.environ["SNOWSQL_WH"] = ""
+os.environ["SNOWSQL_DB"] = ""
+os.environ["SNOWSQL_SCH"] = ""
+
+
+
