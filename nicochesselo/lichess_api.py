0bc26f4c127b9828b2c9662df620e3a49ba46566
Eloi <eloisanchez16@gmail.com>
2023-03-13 11:05:24 +0100

    First working set of game downloading

--- nicochesselo/lichess_api.py
+++ nicochesselo/lichess_api.py
@@ -1,5 +1,10 @@
 import json
 import requests
+import chess.pgn as pgn
+import io
+
+
+DEFAULT_GAME_TYPES = ('classical', 'rapid', 'blitz', 'bullet')
 
 
 def _resp_to_json(response):
@@ -10,16 +15,57 @@ def _get_usernames(json_list, field = 'username'):
     return set(x[field] for x in json_list)
 
 
-def get_leaderboard(n, game_types = ('classical', 'rapid', 'blitz', 'bullet')):
+def get_leaderboard(
+    game_types = ('classical', 'rapid', 'blitz', 'bullet'), n=200
+    ):
+    users = set()
     for game_type in game_types:
-        yield _get_usernames(_resp_to_json(
+        users = users.union(_get_usernames(_resp_to_json(
             requests.get(f'https://lichess.org/api/player/top/{n}/{game_type}')
-            )['users'])
+            )['users']))
+    return users
 
 
 def get_streamers():
-    return _get_usernames(_resp_to_json(
+    return set(_get_usernames(_resp_to_json(
         requests.get(f'https://lichess.org/api/streamer/live')
-        ), 'name')
+        ), 'name'))
 
 
+def get_games(usernames, n = 100):
+    usernames = [usernames] if type(usernames) == str else usernames
+
+    game_info_params = {
+        'max' : f'{n}',
+        'perfType' : ','.join(DEFAULT_GAME_TYPES),
+        'evals' : 'false'
+    }
+
+    games = []
+    for user in usernames:
+        print(f'getting {user}')
+        games.append(_parse_games(
+            requests.get(f'https://lichess.org/api/games/user/{user}',
+                         params = game_info_params)
+        ))
+    return games
+
+
+def _parse_games(response):
+    games = []
+    with io.StringIO(response.text) as games_text:
+        while True:
+            game = pgn.read_game(games_text)
+
+            if game is None:
+                break
+
+            games.append(game)
+    return games
+
+
+def get_default_games(n_users=200, n_games=100):
+    return get_games(
+        get_leaderboard(n=n_users).union(get_streamers()),
+        n=n_games
+        )
--- nicochesselo/main.py
+++ nicochesselo/main.py
@@ -1,1 +1,4 @@
 import lichess_api
+
+
+games = lichess_api.get_default_games(n_users = 5, n_games = 20)
